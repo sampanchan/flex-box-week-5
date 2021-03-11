6894ebad2a3b592d1fe8345454c7466bad143202
sampanchan <spankratz@my.creativecircus.edu>
2021-03-10 21:24:11 -0500

    updated final rough

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,13 @@ Change this message, and make sure it changes in the browser
 to verify that you're working in the right files.`)
 
 
-let toggle = document.querySelector('.toggle')
+// let toggle = document.querySelector('.toggle')
 
-toggle.addEventListener('click', function(event){
-console.log('it works')
-let toggle = document.querySelector('nav')
-toggle.classList.toggle('coolTog')
-// let blurBackground = document.querySelector('.container')
-// blurBackground.style.filter= "blur(10px)"
+// toggle.addEventListener('click', function(event){
+// console.log('it works')
+// let toggle = document.querySelector('nav')
+// toggle.classList.toggle('coolTog')
+// // let blurBackground = document.querySelector('.container')
+// // blurBackground.style.filter= "blur(10px)"
 
-})
+// })
